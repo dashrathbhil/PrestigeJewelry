0b9660ade2fa5540faffb7b68d558a97c7248d6d
dashrath24 <dashrathbhil24@.com>
2025-03-21 12:08:30 +0530

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,36 @@
+//nav
+document.addEventListener("DOMContentLoaded", function () {
+  const toggler = document.querySelector(".navbar-toggler");
+  const menu = document.querySelector(".nav-menu");
+
+  toggler.addEventListener("click", function () {
+    menu.classList.toggle("active");
+  });
+});
+
 // video
 
-const videoBox = document.getElementById('videoBox');
-const thumb = document.getElementById('play-button');
+const videoBox = document.getElementById("videoBox");
+const thumb = document.getElementById("play-button");
 
-thumb.addEventListener('click', () => {
-    videoBox.innerHTML = `<iframe width="100%" height="315" src="https://www.youtube.com/embed/aqz-KE-bpKQ?si=-ovZGrrVpUPfBRiR" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
+thumb.addEventListener("click", () => {
+  videoBox.innerHTML = `<iframe width="100%" height="315" src="https://www.youtube.com/embed/aqz-KE-bpKQ?si=-ovZGrrVpUPfBRiR" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
 });
+
+
+
+
+
+// console.log("Page: " + window.location.pathname);
+
+// let navLinks = document.querySelectorAll("nav a");
+
+// navLinks.forEach(link => {
+// let linkHref = link.getAttribute("href");
+
+// if (window.location.pathname.includes(linkHref)) {
+// link.style.color = "rgb(255, 207, 84)";
+// }
+// });
+
+
